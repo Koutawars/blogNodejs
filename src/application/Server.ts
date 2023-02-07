4486e6e2a571cbcfe8a85b437c3102a065aa1b55
Kevin Urieles <kevin.urieles@bam.com.gt>
2023-02-07 15:30:15 -0500

    initial commit

---
+++ src/application/Server.ts
@@ -0,0 +1,35 @@
+// correr una app de node con ts-node
+
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+export class Server {
+
+    public app: express.Application;
+
+    constructor() {
+        this.app = express(); // inicializar express
+        this.config(); // configurar express
+        this.routes(); // configurar rutas
+    }
+
+    config(): void {
+        this.app.set('port', process.env.PORT || 3000); // configurar puerto
+        this.app.use(morgan('dev')); // configurar morgan
+        this.app.use(cors()); // configurar cors
+        this.app.use(express.json()); // configurar express para recibir json
+        this.app.use(express.urlencoded({extended: false})); // configurar express para recibir datos desde formularios
+    }
+
+    routes(): void {
+        // this.app.use('/api/employees', employeeRoutes);
+    }
+
+    start(): void {
+        this.app.listen(this.app.get('port'), () => { // iniciar servidor
+            console.log('Server on port', this.app.get('port'));
+        });
+    }
+
+}
